2590f5b3c8cd4693b961c634577a2f54dfd13ab6
Jared Kuemmerle <jared.kuemmerel@yahoo.com>
2015-04-10 15:30:54 -0700

    write eventree in php

--- algorithms/game-theory/permutation-game.py
+++ algorithms/game-theory/permutation-game.py
@@ -11,11 +11,30 @@ Assuming both play optimally, who wins the game?
 https://www.hackerrank.com/challenges/permutation-game
 """
 
-def io_transform():
+class InversionGraph():
+    adjacency = {}
+    def __init__(self, perm_list):
+        for key, value in enumerate(perm_list):
+            for i in enumerate(perm_list[key:]:
+
+                # _add_edge(node_1, node_2)
+
+    def _add_edge(self, node_1, node_2):
+        _add_edge_node(self, node_1, node_2)
+        _add_edge_node(self, node_2, node_1)
+
+    def _add_edge_node(self, node_1, node_2)
+        if self.adjacency.has_key(node_1):
+            self.adjacency[node_1].append(node_2):
+        else:
+            self.adjacency[node_1] = [node_2]
+
+def get_input():
     num_cases = int(input())
     for i in range(num_cases):
         _ = int(input())
         list = [int(item) for item in input().split()]
+    return InversionGraph(list)
 
 if __name__ == "__main__":
-    io_transform()
+    graph = get_input()
---
+++ algorithms/graph-theory/eventree/eventree.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * @author Jared
+ */
+
+/*
+Problem:
+You are given a tree (a simple connected graph with no cycles). You have to remove
+as many edges from the tree as possible to obtain a forest with the condition that : 
+Each connected component of the forest should contain an even number of vertices.
+
+Link: https://www.hackerrank.com/challenges/even-tree
+*/
+
+
+class TreeGraph {
+
+    private $_adj_list = array();
+    private $_visited = array();
+
+    public function decompose() {
+        $cuts = 0;
+        foreach (array_keys($this->_adj_list) as $node) {
+            foreach ($this->_adj_list[$node] as $edge_node => $value) {
+                if ($this->_nodes_in_subtree_is_even($node, $edge_node)
+                    && $this->_nodes_in_subtree_is_even($edge_node, $node)) {
+                    $this->_cut($node, $edge_node);
+                    $cuts += 1;
+                }
+            }
+        }
+        return $cuts;
+    }
+
+    public function add_edge($node_1, $node_2) {
+
+        $this->_add_edge_node($node_1, $node_2);
+        $this->_add_edge_node($node_2, $node_1);
+    }
+
+    private function _add_edge_node($node_1, $node_2) {
+
+        if (!isset($this->_adj_list[$node_1])) {
+            $this->_adj_list[$node_1] = array();
+        }
+        $this->_adj_list[$node_1][$node_2] = true;
+    }
+
+    private function _cut($node1, $node2) {
+        unset($this->_adj_list[$node1][$node2]);
+        unset($this->_adj_list[$node2][$node1]);
+    }
+
+    private function _nodes_in_subtree_is_even($the_node, $skip_node=null) {
+        $this->_visited = array($the_node => true);
+        if ($skip_node !== null) {
+            $this->_visited[$skip_node] = true;
+        }
+        return ($this->_number_connected($the_node) + 1) % 2 == 0;
+    }
+
+    private function _number_connected($the_node) {
+        $num_connected = 0;
+        foreach ($this->_adj_list[$the_node] as $node => $value) {
+            
+            if (!isset($this->_visited[$node])) {
+                $this->_visited[$node] = true;
+                $num_connected += $this->_number_connected($node) + 1;
+            }
+        }
+        return $num_connected;
+    }
+
+    private function _degree($node) {
+        return count($this->_adj_list[$node]);
+    }
+}
+
+function readGraph() {
+
+    list($num_vertices, $num_edges) = explode(" ", trim(fgets(STDIN)));
+    $num_vertices = (int) $num_vertices;
+    $num_edges = (int) $num_edges;
+    $graph = new TreeGraph();
+
+    for ($i = 0; $i < $num_edges; $i++) {
+        list($node_1, $node_2) = explode(" ", trim(fgets(STDIN)));
+        $node_1 = (int) $node_1;
+        $node_2 = (int) $node_2;
+        $graph->add_edge($node_1, $node_2);
+    }
+    return $graph;
+}
+
+    $graph = readGraph();
+    $num_cuts = $graph->decompose();
+    echo $num_cuts;
