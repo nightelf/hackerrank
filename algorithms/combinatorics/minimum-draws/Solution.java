5da182bb3f7dba8195b1ac7c3de3fa1829ee97e0
Jared Kuemmerle <jared.kuemmerel@yahoo.com>
2015-04-14 22:40:53 -0700

    steamline java code

--- algorithms/combinatorics/minimum-draws/Solution.java
+++ algorithms/combinatorics/minimum-draws/Solution.java
@@ -6,29 +6,10 @@ public class Solution {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         int pairs, numDraws, numCases;
-        BufferedReader bufferedReader;
-        String line;
-        Scanner in;
+        Scanner in = new Scanner(System.in);
 
-        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        try {
-            line = bufferedReader.readLine();
-            in = new Scanner(line);
-            numCases = in.nextInt();
-        } catch(IOException e) {
-            e.printStackTrace();
-            return;
-        }
+        numCases = in.nextInt();
         for (int i = 0; i < numCases; i++) {
-            try {
-                line = bufferedReader.readLine();
-            } catch(IOException e) {
-                e.printStackTrace();
-                continue;
-            }
-            
-            in = new Scanner(line);
             pairs = in.nextInt();
             numDraws = pairs > 0 ? pairs + 1 : 0;
             System.out.println(numDraws);
